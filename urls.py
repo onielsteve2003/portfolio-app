83e9ab54c8352a14fda7aeb01f6340c029aa09ba
Onah Stephen <67418893+onielsteve2003@users.noreply.github.com>
2021-05-21 12:00:31 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
-    path('', views.homepage),
-    path('count/', views.count, name="count"),
-]
+    path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- views.py
+++ views.py
@@ -1,26 +1,7 @@
-from django .http import HttpResponse
 from django.shortcuts import render
-import operator
 
-def homepage(request):
-    return render(request, 'home.html',)
+from .models import Job
 
-def count(request):
-    fulltext = request.GET['fulltext']
-
-    wordlist = fulltext.split()
-    
-    worddictionary = {}
-
-    for word in worddictionary:
-        if word in worddictionary:
-            # Increase
-            worddictionary[word] += 1
-
-        else:
-            # Add to dictionary
-            worddictionary[word] = 1
-
-    sortedwords= sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
-
-    return render(request, 'count.html', {'fulltext':fulltext, "count" :len(wordlist), 'sortedwords':sortedwords})
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
